cb86439a2fb5c70dd47d69a636704b346326c08a
Hycarus <mirkomarasco08@gmail.com>
2024-01-20 21:37:37 +0100

    update db

--- config/db.php
+++ config/db.php
@@ -5,15 +5,13 @@ return [
             "image" => "https://immagini.editorialedomani.it/version/c:ZDEzODQyM2QtNDhiZS00:ZWRhZTlj/pag_8_fat_peolple_don-t_go_to_heaven-def-jpg.webp?f=3%3A2&q=0.75&w=635",
             "title" => "Boolflix",
             "body" => "A partire da radici antiche la condanna della gola ha avuto una particolare rilevanza nella definizione della buona cristiana e del buon cristiano.<br>Tale condanna si ripete ancora oggi, attraverso l'ossessione per il corpo magro, la colpevolizzazione dell'obesità o anche del semplice sovrappeso.<br>Un punto di osservazione giudicante mette malamente in discussione le persone che soffrono di disturbi alimentari e racconta una storia che ancora oggi fa risuonare la propria eco, soprattutto nelle comunità statunitensi.",
-            "url" => "https://github.com/IIODERII/vite-boolflix",
-            "technologies" => "Vue"
+            "url" => "https://github.com/Hycarus/vite-boolflix",
         ],
         [
             "image" => "https://immagini.editorialedomani.it/version/c:MWU2ZmEyZWEtMDI5MS00:ODAzZjEy/obj92908007_1.webp?f=3%3A2&q=0.75&w=635",
             "title" => "Boolzapp",
             "body" => "Per capire a colpo d'occhio quanto la televisione sia cambiata basta un confronto degli argomenti che a partire dal 2012 hanno offerto la materia al Festival della Tv, che occupa annualmente le piazze di Dogliani nel cuore delle Langhe. Nella prima edizione, tra gli scricchiolii della crisi finanziaria e con i tecnici al governo, la parte principale del proscenio era presa da manager e volti del duopolio Rai-Mediaset, entrambe impegnate, anche dietro l'incalzare di La7, a suonare il piffero anti casta. Argomento allora obbligato, astuto e taumaturgico per radunare pubblico ai talk show.",
-            "url" => "https://github.com/IIODERII/vite-yu-gi-oh",
-            "technologies" => "JavaScript HTML Bootstrap"
+            "url" => "https://github.com/Hycarus/vue-boolzapp",
         ]
     ]
 ];
--- config/technologies.php
+++ config/technologies.php
@@ -30,6 +30,22 @@ return [
         [
             'name' => 'Bootstrap',
             'image' => 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg'
+        ],
+        [
+            'name' => 'Tailwind',
+            'image' => 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg'
+        ],
+        [
+            'name' => 'Git',
+            'image' => 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg'
+        ],
+        [
+            'name' => 'CSS',
+            'image' => 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg'
+        ],
+        [
+            'name' => 'MySQL',
+            'image' => 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg'
         ]
     ]
 ];
